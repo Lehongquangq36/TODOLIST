a67f7967f72e4a7a1e140b247c9e5f3a7c17d88c
quang <lehongquangq36@gmail.com>
2020-10-07 10:59:02 +0700

    Updates

---
+++ precache-manifest.3f2fe638ced78dff3803435881c4ddff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a32f2bf56049dc130517a709bfbdbe0",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "496e8f662ba44a4b6b0c",
+    "url": "/todolist/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ae0bef72dee2b2965384",
+    "url": "/todolist/static/js/2.b17d1927.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/todolist/static/js/2.b17d1927.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "496e8f662ba44a4b6b0c",
+    "url": "/todolist/static/js/main.07b876f4.chunk.js"
+  },
+  {
+    "revision": "197f51c8578d731c6831",
+    "url": "/todolist/static/js/runtime-main.e7725399.js"
+  }
+]);
